033d64992c0929ce6e72080a85ac882a21b1aca8
James Yu <james@Jamess-MacBook-Pro.local>
2021-04-09 16:27:50 -0700

    implemented Cal's code to API

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,79 +1,82 @@
+import random
 class Individual:
-    _chromosome = []
-    _fitness = None
-    MIN_VAL = -100
-    MAX_VAL = 100
 
     def __init__(self, chromosome_len):
-        for(i in range(chromosome_len)):
-            _chromosome.append(random.uniform(MIN_VAL, MAX_VAL))
+        self.chromosome_len = chromosome_len
+        self._chromosome = []
+        self._fitness = None
+        self.MIN_VAL = -100
+        self.MAX_VAL = 100
+        for i in range(chromosome_len):
+            rand_num = random.uniform(self.MIN_VAL, self.MAX_VAL)
+            self._chromosome.append(rand_num)
 
-    def get_fitness():
-        return _fitness
+    def get_fitness(self):
+        return self._fitness
 
-    def set_fitness(new_fitness):
-        fitness = new_fitness
+    def set_fitness(self,new_fitness):
+        self.fitness = new_fitness
 
-    def get_chromosome():
-        return _chromosome.copy()
+    def get_chromosome(self):
+        return self._chromosome[:]
 
-    def set_chromosome(new_chromosome):
-        _chromosome = new_chromosome.copy()
+    def set_chromosome(self,new_chromosome):
+        _chromosome = new_chromosome[:]
 
-    def copy():
-        ret = Individual(chromosome_len)
-        ret.set_fitness(_fitness)
-        ret.set_chromosome(_chromosome)
+    def copy(self):
+        ret = Individual(self.chromosome_len)
+        ret.set_fitness(self._fitness)
+        ret.set_chromosome(self._chromosome)
         return ret
 
-    def mutate(mutation_rate):
+    def mutate(self,mutation_rate):
         '''
         If the mutation rate is less than some randomly chosen value, then we 
         choose a random index and reassign it to a random value. This represents
         the chance for a mutation in an offspring
         '''
         if(random.uniform(0,1) > mutation_rate):
-            _chromosome[int(random.uniform(0,len(_chromosome)))] = random.uniform(MAX_VAL, MIN_VAL)
+            self._chromosome[int(random.uniform(0,len(self._chromosome)))] = random.uniform(self.MAX_VAL, self.MIN_VAL)
 
 class GeneticAlgorithm:
-    population = []
-    population_size = None
-    chromosome_len = None
-    generation = None
-    best_individual = None
-    mutation_rate = 0.8
     
     def __init__(self, size, rate, length):
+        self.population = []
+        self.population_size = None
+        self.chromosome_len = None
+        self.generation = 0
+        self.best_individual = None
+        self.mutation_rate = 0.8
         # Set class variables
-        population_size = size
-        mutation_rate = rate
-        chromosome_len = length
+        self.population_size = size
+        self.mutation_rate = rate
+        self.chromosome_len = length
 
         # Generate population
-        for(i in range(population_size)):
-            population.append(Individual(chromosome_len))
+        for i in range(self.population_size):
+            self.population.append(Individual(self.chromosome_len))
 
-    def get_population():
-        return population.copy()
+    def get_population(self):
+        return self.population[:]
     
-    def get_best_individual():
-        return best_individual.copy()
+    def get_best_individual(self):
+        return self.best_individual.get_chromosome()
     
-    def set_individual_fitness(index, fitness):
-        population[index].set_fitness(fitness)
+    def set_individual_fitness(self,index, fitness):
+        self.population[index].set_fitness(fitness)
 
-    def crossover(parent1, parent2):
+    def crossover(self,parent1, parent2):
         '''
         This function generates a new individual by randomly choosing hyperparameters
         from two parents and then mutating randomly
         '''
-        child = Individual(chromosome_len)
+        child = Individual(self.chromosome_len)
         chromosome1 = parent1.get_chromosome()
         chromosome2 = parent2.get_chromosome()
         child_chromosome = []
 
         # Randomly choose hyperparameters from parents
-        for(i in range(chromosome_len)):
+        for i in range(self.chromosome_len):
             if(random.uniform(0,1) > 0.5):
                 child_chromosome.append(chromosome1[i])
             else:
@@ -82,31 +85,31 @@ class GeneticAlgorithm:
         child.set_chromosome(child_chromosome)
 
         # Mutate the child
-        child.mutate(mutation_rate)
+        child.mutate(self.mutation_rate)
 
         return child
 
-    def new_generation():
+    def new_generation(self):
 
         # Sort the current population by fitness
-        sort_by_fitness()
+        self.sort_by_fitness()
 
         # Save the best individual
-        if(best_individual is None or population[0].getFitness() > best_individual.getFitness()):
-            best_individual = population[0].copy()
+        if(self.best_individual is None or self.population[0].get_fitness() > self.best_individual.get_fitness()):
+            self.best_individual = self.population[0].copy()
 
         # Take the top half of the population to form the new population
         # Note: this can be changed to something else if we think of a better way
-        fit_population = population[:population_size/2].copy()
+        self.fit_population = self.population[:self.population_size/2][:]
 
         # Randomly choose parents to crossover for a new generation
         population = []
-        for(i in range(population_size)):
-            population.append(crossover(fit_population[int(random.uniform(0,len(fit_population)))], fit_population[int(random.uniform(0,len(fit_population)))]))
+        for i in range(self.population_size):
+            population.append(self.crossover(self.fit_population[int(random.uniform(0,len(self.fit_population)))], self.fit_population[int(random.uniform(0,len(self.fit_population)))]))
 
-        generation+=1
+        self.generation+=1
 
-    def sort_by_fitness():
+    def sort_by_fitness(self):
         '''
         This function sorts the population by fitness value.
 
@@ -115,7 +118,7 @@ class GeneticAlgorithm:
         they are more than welcome to, however I do not think that it will be 
         worth while.
         '''
-        for(i in range(chromosome_len-1)):
-            for(j in range(chromosome_len-i-1)):
-                if(population[j].get_fitness() > population[j+1].get_fitness()):
-                    population[j], population[j+1] = population[j+1], population[j]
+        for i in range(self.population_size-1):
+            for j in range(self.population_size-i-1):
+                if(self.population[j].get_fitness() > self.population[j+1].get_fitness()):
+                    self.population[j], self.population[j+1] = self.population[j+1], self.population[j]
--- vrep_main.py
+++ vrep_main.py
@@ -3,6 +3,13 @@ import sys
 import math
 import time
 import random
+import GeneticAlgorithm
+
+pop_size = 10
+mut_rate = .5
+num_params = 6
+genAlg = GeneticAlgorithm.GeneticAlgorithm(pop_size, mut_rate, num_params)
+
 
 vrep.simxFinish(-1) # just in case, close all opened connections
 clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to V-REP 5 last param is 200 hz
@@ -32,58 +39,72 @@ for x in range(1,5):
     horizontal_joint_handles.append(temp_h_handle)
     verticle_joint_handles.append(temp_v_handle)
 
-error_code, gps_handle = vrep.simxGetObjectHandle(clientID, "GPS", vrep.simx_opmode_oneshot_wait)
-# print(gps_handle)
-#
-# print(horizontal_joint_handles)
-# print(verticle_joint_handles)
+vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
+time.sleep(1)
+num_gen = 5
 
-time_step = 0
-
-speed = 5
-ampitude_h = 0
-ampitude_v = 20
-phase_v = 120
-phase_h = 160
-phase_cam = 180
-encodings = [speed, ampitude_h, ampitude_v, phase_v, phase_h]
-
-A_H= ampitude_h*math.pi/180
-A_V= ampitude_v*math.pi/180
-P_V= phase_v*math.pi/180
-P_H=phase_h*math.pi/180
-P_C=phase_cam*math.pi/180
-inputs = [A_H, A_V, P_V, P_H, P_C]
-while True:
-    time_step += 1
-
-
-
-    for x in range(1,5):
-        error_code = vrep.simxSetJointTargetPosition(clientID,
-                                                     verticle_joint_handles[x-1],
-                                                     (A_V)*math.sin(time_step*(speed)+x*(P_V)),
-                                                     vrep.simx_opmode_oneshot)
-        error_code = vrep.simxSetJointTargetPosition(clientID,
-                                                     horizontal_joint_handles[x-1],
-                                                     (A_H)*math.cos(time_step*(speed)+x*(P_H)),
-                                                     vrep.simx_opmode_oneshot)
-
-    error_code = vrep.simxSetJointTargetPosition(clientID,
-                                                 camera_motor_handle,
-                                                 (A_V)/2*math.sin(time_step*(speed)+(P_V)+(P_C)),
-                                                 vrep.simx_opmode_oneshot)
-    if time_step % 200 == 0:
-        error_code, position = vrep.simxGetObjectPosition(clientID, camera_motor_handle, -1, vrep.simx_opmode_blocking)
-        print(position[:2])
-        vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
-        time.sleep(1)
+for i in range(num_gen):
+    print("new generation " + i)
+    pop = genAlg.get_population()
+    for j in range(pop_size):
+        print("new individual")
         vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
+        temp_individual = pop[j]
+        temp_chroms = temp_individual.get_chromosome()
+        print("chroms:")
+        print(temp_chroms)
 
-    time.sleep(.05)
-    if error_code != 0 and error_code != 1:
-        print(error_code)
-        sys.exit()
+        time_step = 0
+
+        speed = temp_chroms[0]
+        ampitude_h = temp_chroms[1]
+        ampitude_v = temp_chroms[2]
+        phase_v = temp_chroms[3]
+        phase_h = temp_chroms[4]
+        phase_cam = temp_chroms[5]
+        encodings = [speed, ampitude_h, ampitude_v, phase_v, phase_h]
+
+        A_H= ampitude_h*math.pi/180
+        A_V= ampitude_v*math.pi/180
+        P_V= phase_v*math.pi/180
+        P_H=phase_h*math.pi/180
+        P_C=phase_cam*math.pi/180
+        inputs = [A_H, A_V, P_V, P_H, P_C]
+        while True:
+            time_step += 1
 
 
+            for x in range(1,5):
+                error_code = vrep.simxSetJointTargetPosition(clientID,
+                                                             verticle_joint_handles[x-1],
+                                                             (A_V)*math.sin(time_step*(speed)+x*(P_V)),
+                                                             vrep.simx_opmode_oneshot)
+                error_code = vrep.simxSetJointTargetPosition(clientID,
+                                                             horizontal_joint_handles[x-1],
+                                                             (A_H)*math.cos(time_step*(speed)+x*(P_H)),
+                                                             vrep.simx_opmode_oneshot)
+
+            error_code = vrep.simxSetJointTargetPosition(clientID,
+                                                         camera_motor_handle,
+                                                         (A_V)/2*math.sin(time_step*(speed)+(P_V)+(P_C)),
+                                                         vrep.simx_opmode_oneshot)
+
+            # get the coord in 10 seconds
+            if time_step % 200 == 0:
+                error_code, position = vrep.simxGetObjectPosition(clientID, camera_motor_handle, -1, vrep.simx_opmode_blocking)
+                genAlg.set_individual_fitness(j,position[1])
+                print("fitness for individual {}: {}".format(j,position[1]))
+                vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
+                time.sleep(2)
+                break
+
+
+            time.sleep(.05)
+            if error_code != 0 and error_code != 1:
+                print(error_code)
+                sys.exit()
+    genAlg.new_generation()
+
+print("best indidual: ")
+print(genAlg.get_best_individual())
 print("end")
